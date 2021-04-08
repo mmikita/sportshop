0573f9fbbdce5cbcf233844f2d90c6cb8b1b585a
BTN1756\BRITENET <michal.mikita@britenet.com.pl>
2021-04-08 14:36:19 +0200

    #2 - handle apex call exception in toast message

--- src/aura/SS_ProductView/SS_ProductViewController.js
+++ src/aura/SS_ProductView/SS_ProductViewController.js
@@ -1,20 +1,26 @@
 ({
-	doInit : function(component, event, helper) {
-	    var getProduct=component.get("c.getProduct");
+    doInit : function(component, event, helper) {
+        var getProduct=component.get("c.getProduct");
         var productId = component.get("v.recordId");
         getProduct.setParams({"productId": productId});
         getProduct.setCallback(this,function(response) {
             var state= response.getState();
             if(state==='SUCCESS'){ 
-               var temp=response.getReturnValue();
-               component.set("v.productItem",temp);
-               var imagesURLs = component.get("v.productItem").Product2.productImages__c.split(";");
-               component.set("v.images", imagesURLs);
+                var temp=response.getReturnValue();
+                component.set("v.productItem",temp);
+                var imagesURLs = component.get("v.productItem").Product2.productImages__c.split(";");
+                component.set("v.images", imagesURLs);
             }
             else {
-                console.log("Failed with state: " + state);
+                let errors = response.getError();
+                let message = $A.get('$Label.c.SS_unknowError'); 
+                if (errors && Array.isArray(errors) && errors.length > 0) {
+                    message = errors[0].message;
+                }
+                console.log('message' + message);
+                component.find("errorsNotify").setErrorToast(message);
             }
         });
         $A.enqueueAction(getProduct);  
-	}
+    }
 })
